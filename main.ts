37b214166582a87c1c0ef7ff62a97458908b7ee7
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-03-08 10:13:35 -0800

    Q 35 of Ts/js & node js of 45 assignment.
    Q 35 of Ts/js & node js of 45 assignment.

---
+++ main.ts
@@ -0,0 +1,13 @@
+let animalsNames =["Cow","Sheep","Goat","Camel"];
+
+
+//printing only names
+for(let i of animalsNames){
+    console.log(i);
+}
+
+for(let j  of animalsNames){
+    console.log(`${j} is a Domestic Animal`);
+
+}
+console.log(`All thses animals ${animalsNames[0]}, ${animalsNames[1]}, ${animalsNames[2]} and ${animalsNames[3]} meet is Halal in Islam`);
